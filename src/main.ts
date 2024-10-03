f5505dab4d4402beac3dce896f040657d2683ba4
DESKTOP-2UESDE7\jaran <jaran.dch@gmail.com>
2024-10-03 10:31:16 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   app.enableCors({
     origin: "http://localhost:3000", // อนุญาตแค่โดเมนนี้ หรือใช้ '*' เพื่ออนุญาตทุกโดเมน
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true, // กำหนดว่าต้องส่ง cookies กับ request หรือไม่
+    credentials: true, // กำหนดว่าต้องส่ง cookies กับ request หรือไม่ test
   });
   await app.listen(3001);
 }
