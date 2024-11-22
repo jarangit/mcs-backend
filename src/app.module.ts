893d8591bd8dda9846fbb99c84f2cf491f52009a
Jaran Donchaaim <>
2024-11-23 00:58:23 +0700

    fix: update MySQL service type and refactor database configuration to use environment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,49 +17,55 @@ import { CollectionModule } from "./collection/collection.module";
 import { ConfigModule } from "@nestjs/config";
 import { StCategoryModule } from "./system-data/st-category/st-category.module";
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localhost",
-      port: 3366,
-      username: "mcs_user", // เปลี่ยนเป็นชื่อผู้ใช้ของคุณ
-      password: "1234", // เปลี่ยนเป็นรหัสผ่านของคุณ
-      database: "mcs_db", // เปลี่ยนเป็นชื่อฐานข้อมูลของคุณ
-      entities: [__dirname + "/**/*.entity{.ts,.js}"], // ตั้งค่าที่อยู่ของ entities
-      synchronize: true, // ตั้งค่า true ใน dev environment เท่านั้น
-    }),
-    UsersModule,
-    ProductModule,
-    CategoryModule,
-    LikeModule,
-    UtilsModule,
-    PublicProductModule,
-    UserModule,
-    CollectionModule,
-    StCategoryModule,
-  ],
-  controllers: [AppController],
-  providers: [
-    AppService,
-    JwtService,
-    UtilsService,
-    // {
-    //     provide: APP_GUARD,
-    //     useClass: HeaderGuard,
-    // },
-    // {
-    //     provide: APP_INTERCEPTOR,
-    //     useClass: HeadersInterceptor,
-    // },
-  ],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        // TypeOrmModule.forRoot({
+        //     type: "mysql",
+        //     host: "localhost",
+        //     port: 3366,
+        //     username: "mcs_user", // เปลี่ยนเป็นชื่อผู้ใช้ของคุณ
+        //     password: "1234", // เปลี่ยนเป็นรหัสผ่านของคุณ
+        //     database: "mcs_db", // เปลี่ยนเป็นชื่อฐานข้อมูลของคุณ
+        //     entities: [__dirname + "/**/*.entity{.ts,.js}"], // ตั้งค่าที่อยู่ของ entities
+        //     synchronize: true, // ตั้งค่า true ใน dev environment เท่านั้น
+        // }),
+        TypeOrmModule.forRoot({
+            type: "mysql",
+            url: process.env.DATABASE_URL,
+            autoLoadEntities: true,
+            synchronize: true, // ควรปิดใน Production
+        }),
+        UsersModule,
+        ProductModule,
+        CategoryModule,
+        LikeModule,
+        UtilsModule,
+        PublicProductModule,
+        UserModule,
+        CollectionModule,
+        StCategoryModule,
+    ],
+    controllers: [AppController],
+    providers: [
+        AppService,
+        JwtService,
+        UtilsService,
+        // {
+        //     provide: APP_GUARD,
+        //     useClass: HeaderGuard,
+        // },
+        // {
+        //     provide: APP_INTERCEPTOR,
+        //     useClass: HeadersInterceptor,
+        // },
+    ],
 })
 export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(HeadersMiddleware, LoggerMiddleware)
-      .forRoutes({ path: "auth/*", method: RequestMethod.ALL }); // ใช้กับทุก route
-  }
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(HeadersMiddleware, LoggerMiddleware)
+            .forRoutes({ path: "auth/*", method: RequestMethod.ALL }); // ใช้กับทุก route
+    }
 }
